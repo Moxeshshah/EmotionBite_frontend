53ef16d7b1cd43e4cd4a4f217550a51d446b1c7a
Moxesh Shah <shahmoxesh156@gmail.com>
2026-03-24 10:30:52 +0530

    fix build error

--- app/propose/receiver/page.jsx
+++ app/propose/receiver/page.jsx
@@ -1,6 +1,5 @@
 import { Suspense } from "react";
-import CongratsReceiverClient from "@/components/CongratsReceiverClient";
-import LoveReceiver from "./LoveReceiverClient";
+
 import ProposalReceiver from "./ProposeReceiverClient";
 
 export default function Page() {
